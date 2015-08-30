16e22a81c76990511c77b7241311fc7e5f8db266
dantleech <dan.t.leech@gmail.com>
2015-08-30 09:23:22 +0200

    Docs

--- lib/Definition.php
+++ lib/Definition.php
@@ -11,34 +11,73 @@
 
 namespace PhpBench\Tabular;
 
+/**
+ * Object representing the table definition, implements ArrayAccess.
+ */
 class Definition extends \ArrayObject
 {
+    /**
+     * @var integer[]
+     */
     private $passes = array();
+
+    /**
+     * @var string[]
+     */
     private $columnNames = array();
+
+    /**
+     * @var string
+     */
     private $path;
 
+    /**
+     * @param array $definition
+     * @param string $path
+     */
     public function __construct(array $definition, $path = null)
     {
         $this->path = $path;
         parent::__construct($definition);
     }
 
+    /**
+     * Set the column names and compiler passes.
+     *
+     * @param array $columnNames
+     * @param array $passes
+     */
     public function setMetadata(array $columnNames, array $passes)
     {
         $this->columnNames = $columnNames;
         $this->passes = $passes;
     }
 
+    /**
+     * Return the path that included files should be relative to.
+     *
+     * @return string
+     */
     public function getBasePath()
     {
         return dirname($this->path);
     }
 
+    /**
+     * Return the compiler pass numbers.
+     *
+     * @return integer[]
+     */
     public function getPasses()
     {
         return $this->passes;
     }
 
+    /**
+     * Return the column names of this definition
+     *
+     * @return string[]
+     */
     public function getColumnNames()
     {
         return $this->columnNames;
